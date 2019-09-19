364fef65b597171c0d577a8c1952a1af06667e2a
RockyXRQ <1315930212@qq.com>
2019-09-20 00:12:52 +0800

    确认素数的函数

---
+++ Q12/Prime.h
@@ -0,0 +1,8 @@
+#ifndef _PRIME_H_
+#define _PRIME_H_
+
+#pragma once
+
+int iPrime();
+
+#endif
--- Q12/Q12.cpp
+++ Q12/Q12.cpp
@@ -1,1 +1,12 @@
 #include "Include_List.h"
+#include "Prime.h"
+
+int iPrime();
+
+int main(){
+   iPrime();
+   system("pause");
+   return 0;
+}
+
+
