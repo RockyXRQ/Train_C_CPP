ee759a994f59373e9903e911d684e0d35fb6374e
RockyXRQ <1315930212@qq.com>
2019-09-29 15:02:51 +0800

    zz c homework

---
+++ Q16/main.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>      
+int main(){  
+     int a,b,sum;              //声明部分，定义3个整型变量
+     a=32767;                //对变量a赋值
+     b=456;          
+     sum=a+b;                //进行a+b运算，把结果存放在变量sum中
+     printf("sum is: %d\n",sum);   //输出结果。d表示用十进制整数输出 
+     a=789;
+     sum=a+b;                 //进行a+b运算，把结果存放在变量sum中
+     printf("sum is: %d\n",sum);   //输出结果。d表示用十进制整数输出 
+     system("pause"); 
+     return 0;   
+}
