003214d33a8ae7fc8ec33dbec42eeb71e7b29dfb
RockyXRQ <1315930212@qq.com>
2019-09-29 16:00:11 +0800

    .

---
+++ Q25/program3-1.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+int main(){   
+printf("\tIntroduce yourself\n\n");
+printf("\t******************\n");
+printf("\t  I am a student! \n ");
+printf("\t******************\n");
+system("pause");
+return 0;
+}
