fb9bb1596702f777c9ee5581fd4c1bacc9c45b85
RockyXRQ <1315930212@qq.com>
2019-10-11 20:49:27 +0800

    符合字面量

---
+++ Q29/program7-3.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float waterYield,getTax(register float yield);
+
+float *rangeList=(float[2]){220,80};
+float *rateList=(float[3]){3.45,4.83,5.83};
+
+int main(){
+printf("请输入本月所用的水量：");
+scanf("%f",&waterYield);
+
+printf("您要缴纳的税费为：%.2f",getTax(waterYield));
+
+system("pause");
+return 0;
+}
+
+float getTax(register float yield){
+ register int count=0;
+ register float fund=0;
+ while(1){
+     if(yield-rangeList[count]>0&&count<=1){
+        fund+=rangeList[count]*rateList[count];
+        yield-=rangeList[count];
+        count++;
+      }
+     else{
+        fund+=yield*rateList[count];
+        break;
+    }
+  }
+ return fund;
+ }
+
