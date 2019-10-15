7c7bcacbb0b2965b734570e00495f929dd751f9c
RockyXRQ <1315930212@qq.com>
2019-10-15 14:05:14 +0800

    Bug fixed

--- Q29/program7-3.cpp
+++ Q29/program7-3.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float waterYield,getTax(register float yield);
+float salaryYield,getTax(register float yield);
 
-float *rangeList=(float[2]){220,80};
-float *rateList=(float[3]){3.45,4.83,5.83};
+float *rangeList=(float[6]){1500,3000,4500,26000,20000,25000};
+float *rateList=(float[7]){0.03,0.1,0.2,0.25,0.3,0.35,0.45};
 
 int main(){
-printf("请输入本月所用的水量：");
-scanf("%f",&waterYield);
-
-printf("您要缴纳的税费为：%.2f",getTax(waterYield));
+printf("请输入本月所用的工资：");
+scanf("%f",&salaryYield);
+salaryYield-=3500;
+printf("您要缴纳的税费为：%.2f元",getTax(salaryYield));
 
 system("pause");
 return 0;
@@ -19,6 +19,8 @@ return 0;
 float getTax(register float yield){
  register int count=0;
  register float fund=0;
+ if(yield<=0)
+     return 0;
  while(1){
      if(yield-rangeList[count]>0 && count<=1){
         fund+=rangeList[count]*rateList[count];
