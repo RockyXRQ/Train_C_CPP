57d12031cc659fb76a5c497dfd1a5f3e62142b84
RockyXRQ <1315930212@qq.com>
2019-10-16 23:44:15 +0800

    还差逐行读取

--- Q35/8-5.cpp
+++ Q35/8-5.cpp
@@ -3,20 +3,20 @@
 #include <string.h>
 #include <time.h>
 
-int gerLines(FILE*);
+int getLines(FILE*);
 void listInit(FILE*,struct students*);
 
 struct students{
   bool isCalled=false;
-  char name[20]={0};
-  char stdnNum[20]={0};
+  char name[20];
+  char stdnNum[20];
 };
 
 int main(){    
   printf("现在开始随机点名！（按回车显示入下一个名字）");
   register int calledNum=0;
   FILE *fp=fopen("names.txt","r");
-  register int lineLen=gerLines(fp);
+  register int lineLen=getLines(fp);
   struct students stdnList[lineLen];
   
   listInit(fp,stdnList);  
@@ -26,15 +26,15 @@ int main(){
   unsigned int randNum =rand()%lineLen;
   if(stdnList[randNum].isCalled!=true)
     getchar();
-    printf("现在被叫到的是：%s\n",stdnList[randNum].name);
+    printf("现在被叫到的是：%s",stdnList[randNum].stdnNum);
     calledNum+=1;
   }
-  printf("所有名字已经被点完。");
+  printf("所有名字已经被点完。\n");
   system("pause");
   return 0;
 }
 
-int gerLines(FILE* fp1){
+int getLines(FILE* fp1){
   register int lines=0;
   while(!feof(fp1)){
    if(fgetc(fp1)=='\n')
@@ -44,7 +44,7 @@ int gerLines(FILE* fp1){
 }
 
 void listInit(FILE* fp,struct students* stdnList){
-  for(int count=0;count<gerLines(fp);count++){
+  for(int count=0;count<getLines(fp);count++){
     fscanf(fp,"%s%s",stdnList[count].stdnNum,stdnList[count].name);
   }
 }
