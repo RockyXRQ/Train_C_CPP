aff86a27a716fe1345c87fcf2c67cc976c4fb9c1
RockyXRQ <1315930212@qq.com>
2019-11-27 20:56:06 +0800

    Homework.

--- Q80/Q1.cpp
+++ Q80/Q1.cpp
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int IsRight(int, int);
-float invaildValue = 0;
+int Money[100] = {0};
 
 int main() {
-        for (register int qty1 = 0; qty1 <= 30; ++qty1)
-        for (register int qty2 = 0; qty2 <= 10; ++qty2)
-            if (IsRight(qty1, qty2)) printf("±ÒÖµÎª£º%.2f\n", invaildValue);
+    for (register int qty1 = 0; qty1 <= 30; ++qty1)
+        for (register int qty2 = 0; qty2 <= 10; ++qty2) {
+            register int tempValue = 5 * qty1 + 2 * qty2;
+            if (tempValue<100 & tempValue> 1) Money[tempValue] = 1;
+        }
+    for (register int target = 1; target < 100; ++target)
+        if (!Money[target]) printf("%.2fÔª\n", (float) target * 0.01);
     system("pause");
-    return 0;
-}
-
-int IsRight(int qty1, int qty2) {
-    invaildValue = (float)qty1 * 0.02 + (float)qty2 * 0.05;
-    if (invaildValue > 1.00 || invaildValue < 0.01) {
-        return 1;
-    }
     return 0;
 }
