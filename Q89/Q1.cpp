16623f1cdff7ed03fb1f8c907d4367a72ee02b15
RockyXRQ <1315930212@qq.com>
2019-12-08 23:24:49 +0800

    Homework.

---
+++ Q89/Q1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <typeinfo>
+#include <cstdlib>
+#include <array>
+
+int main() {
+    std::array<int, 3> arr = {1, 2, 3};
+    system("pause");
+    return 0;
+}
---
+++ Q90/Q1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <typeinfo>
+#include <cstdlib>
+
+struct Year {
+    int year = 0;
+};
+
+int main() {
+    Year arr2[3];
+    Year** arr1 = new Year*[3];
+    for (register int i = 0; i < 3;++i){
+        arr1[i] = new Year;
+    } 
+    
+    Year** p1 = arr1;
+    Year* p2 = arr2;
+    std::cout << (*p1)->year << std::endl;
+    std::cout << (*p2).year << std::endl;
+    
+    system("pause");
+    return 0;
+}
